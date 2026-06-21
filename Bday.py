7f4102b53a40559b34dec2245d8f9b358b5681a5
arnavneerajverma123-afk <arnav.neeraj.verma123@gmail.com>
2026-06-21 21:24:53 +0530

    Add files via upload

---
+++ Bday.py
@@ -0,0 +1,10 @@
+Ruchi="Ruchi bday 11/3/1978"
+Neeraj="Neeraj bday 3/11/1978"
+Arnav="Arnav bday 18/2/2008"
+Kairav="Kairav bday 21/10/2013"
+Sujay="Sujay bday 2/11/2013"
+print(Ruchi)
+print(Neeraj)
+print(Arnav)
+print(Kairav)
+print(Sujay)
