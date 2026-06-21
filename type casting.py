78d56241e6c3f339ce6ee33eeb796b1f3753d7a9
arnavneerajverma123-afk <arnav.neeraj.verma123@gmail.com>
2026-06-21 20:00:03 +0530

    Add files via upload

---
+++ type casting.py
@@ -0,0 +1,23 @@
+name="penguin"
+age=15
+is_student=True
+weight=38.5
+print("name :", name)
+print("Data Type of Name is",type(name))
+
+print("Age :", age)
+print("Data Type of Age is",type(age))
+
+print("is_student:")
+print("Data Type of is_student is",type(is_student))
+
+print("Weight:",weight)
+print("Data Type of,"type(age))
+
+print("\n After Type casting. . . .")
+age=str(age)
+print(age)
+print("Data Type of,"type(age))
+weight=int(weight)
+print(weight)
+print("Data Type of,"type(weight))
