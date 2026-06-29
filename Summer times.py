ce817bd5aaacbb82b4fe9539dfad9fecad73f6a6
arnavneerajverma123-afk <arnav.neeraj.verma123@gmail.com>
2026-06-29 21:23:29 +0530

    Add files via upload

---
+++ Summer times.py
@@ -0,0 +1,8 @@
+i=(input("is it hot or cold"))
+if(i=="hot","HOT"or"Hot"):
+    print("I will go for a swim")
+elif(i=="cold","COLD"or"Cold"):
+    print("I will drink hot chocolate")
+else:
+    print("incorrect data")
+
