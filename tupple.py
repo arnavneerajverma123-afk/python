6baaf0ef4eb243554f6dda68494a75522eb535ea
arnavneerajverma123-afk <arnav.neeraj.verma123@gmail.com>
2026-09-12 20:36:39 +0530

    Add files via upload

---
+++ tupple.py
@@ -0,0 +1,13 @@
+tuplex = ("tuple",False,3.2,1)
+print(tuplex)
+tuplex=(4,6,8,0,1)
+print(tuplex)
+tuplex = tuplex + (9,)
+print(tuplex)
+tuple1 = (50,30,90,50)
+print(tuple1.count(50))
+tuplex=(4,6,9,8,7,1,0,6)
+_slice = tuplex[3:5]
+print(_slice)
+_slice = tuplex [:6]
+print(_slice)
